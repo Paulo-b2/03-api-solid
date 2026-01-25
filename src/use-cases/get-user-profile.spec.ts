9286ff7127ffa0785dcd91be80972d24311a5033
Paulo Eduardo Nunes Ribero <eduardo.paulob2@gmail.com>
2026-01-25 17:30:13 -0300

    feat: implement GetUserProfile use case with error handling and tests

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository.js";
+import { hash } from "bcryptjs";
+import { GetUserProfileUseCase } from "./get-user-profile.js";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error.js";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: GetUserProfileUseCase;
+
+describe("Get User Profile Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileUseCase(usersRepository);
+  });
+
+  it("should be able to get user profile", async () => {
+    const createUser = await usersRepository.create({
+      name: "Jhon Doe",
+      email: "jhondoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { user } = await sut.execute({
+      userId: createUser.id,
+    });
+
+    expect(user.name).toEqual("Jhon Doe");
+  });
+
+  it("should not be able to get user profile with wrong id", async () => {
+    await expect(
+      sut.execute({
+        userId: "non-existing-id",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
