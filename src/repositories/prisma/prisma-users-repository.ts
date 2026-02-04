d5539bfd278057bf27b11a5f4597fa7aa4546286
Paulo Eduardo Nunes Ribero <eduardo.paulob2@gmail.com>
2026-02-04 19:24:02 -0300

    feat: implement PrismaCheckInsRepository with CRUD operations and user-specific queries

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,18 @@
 import { prisma } from "@/lib/prisma.js";
-import { Prisma, type User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import type { UsersRepository } from "../users-repository.js";
 
 //estou usando da vantagem do prisma de já criar os tipos que podem ser passados para criar uma entidade, dessa maneira eu não preciso criar novamente outra interface para indicar quais os dados eu devo passar como parâmetro no método
 
 export class PrismaUsersRepository implements UsersRepository {
-  findById(id: string): Promise<User | null> {
-    throw new Error("Method not implemented.");
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
   }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
