05578013e31b611a594a586dc06bedadd2f1754e
Paulo Eduardo Nunes Ribero <eduardo.paulob2@gmail.com>
2026-02-12 20:13:08 -0300

    feat: implement nearby and search gym controllers with validation

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import type { FastifyInstance } from "fastify";
 
 import { verifyJwt } from "../../middlewares/verify-jwt.js";
 
+import { search } from "./search.js";
+import { nearby } from "./nearby.js";
+import { create } from "./create.js";
+
 export async function gymsRoutes(app: FastifyInstance) {
-  app.addHook('onRequest', verifyJwt)
+  app.addHook("onRequest", verifyJwt);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+
+  app.post("/gyms", create);
 }
